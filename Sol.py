2dd63c65a9017698b759cd8e4af87b460e07b048
Liz-Conway <zil.conway@gmail.com>
2021-12-11 15:28:01 +0000

    FEAT: Add Sol class with date & weather for a day

---
+++ Sol.py
@@ -0,0 +1,21 @@
+'''
+Created on 1 Dec 2021
+
+@author: Liz-Conway
+'''
+
+class Sol():
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, earth_date, temperature, opacity, wind_speed):
+        '''
+        Constructor
+        '''
+        self.earth_date = earth_date
+        self.temperature = temperature
+        self.opacity = opacity
+        self.wind_speed = wind_speed
+        
