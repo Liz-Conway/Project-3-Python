83c4d18d4981b4ae6c8889bc9d0aaa047e9b9dc0
Liz-Conway <zil.conway@gmail.com>
2021-12-11 18:54:29 +0000

    FEAT: Check input date in the correct range

--- DateValidator.py
+++ DateValidator.py
@@ -5,6 +5,7 @@ Created on 5 Dec 2021
 '''
 
 from datetime import datetime
+from datetime import date
 
 class DateValidator():
     '''
@@ -17,13 +18,19 @@ class DateValidator():
         Constructor
         '''
         self.format = "%Y-%m-%d"
+        self.min_date = date(2012, 8, 7)
+        self.max_date = date(2018, 2, 27)
 
     def validateDate(self, date_string):
         valid = False
         
         validFormat = self.validDateFormat(date_string)
         
-        valid = validFormat
+        validRange = False
+        if(validFormat == True) :
+            validRange = self.validDateRange(date_string)
+        
+        valid = validFormat and validRange
         
         return valid
         
@@ -36,4 +43,15 @@ class DateValidator():
             validated = False
             
         return validated
+    
+    def validDateRange(self, date_string):
+        validRange = False
+        
+        date_time = datetime.strptime(date_string, self.format)
+        date_date = datetime.date(date_time)
+        
+        if date_date >= self.min_date and date_date <= self.max_date :
+            validRange = True
+        
+        return validRange
             
